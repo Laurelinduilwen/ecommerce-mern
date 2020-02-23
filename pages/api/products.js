a98a5cc52fda3168eab24ce5f2b354cc528d9792
Laurelinduilwen <ladylaurelin@hotmail.com>
2020-02-23 05:29:17 +0100

    third commit

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,9 +1,11 @@
+import Product from '../../models/Product';
 import products from '../../static/products.json';
 
 import connectDb from '../../utils/connectDb';
 
 connectDb();
 
-export default (req, res) => {
+export default async (req, res) => {
+  const products = await Product.find();
   res.status(200).json(products);
 };
