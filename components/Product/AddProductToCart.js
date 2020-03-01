176948fed1eacf3e13f28906151ecfcb4a0ff227
Laurelinduilwen <ladylaurelin@hotmail.com>
2020-03-01 03:50:44 +0100

    created individual product cards & fixed environment variable for baseURL

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -1,5 +1,15 @@
+import { Input } from 'semantic-ui-react';
+
 function AddProductToCart() {
-  return <>AddProductToCart</>;
+  return (
+    <Input
+      type="number"
+      min="1"
+      value={1}
+      placeholder="Quantity"
+      action={{ color: 'orange', content: 'Add to Cart', icon: 'plus cart' }}
+    />
+  );
 }
 
 export default AddProductToCart;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 import axios from "axios";
 import ProductList from "../components/Index/ProductList";
+import baseUrl from "../utils/baseUrl";
 
 function Home({ products }) {
   return <ProductList products={products} />;
 }
 
 Home.getInitialProps = async () => {
+  const url = `${baseUrl}/api/products`;
   // fetch data on server
-  const url = "http://localhost:3000/api/products";
   const response = await axios.get(url);
-  return { products: response.data };
   // return response data as an object
+  return { products: response.data };
   // note: this object will be merged with existing props
 };
 
