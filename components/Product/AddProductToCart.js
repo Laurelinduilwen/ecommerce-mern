8089363d906e6d45763130f2ac307353e244979f
Laurelinduilwen <ladylaurelin@hotmail.com>
2020-07-08 01:51:00 +0200

    added cart functionality to the backend

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -1,17 +1,74 @@
-import { Input } from "semantic-ui-react";
+import React from 'react';
+import { Input } from 'semantic-ui-react';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+import baseUrl from '../../utils/baseUrl';
+import catchErrors from '../../utils/catchErrors';
+import cookie from 'js-cookie';
 
-function AddProductToCart() {
+function AddProductToCart({ user, productId }) {
+  const [quantity, setQuantity] = React.useState(1);
+  const [loading, setLoading] = React.useState(false);
+  const [success, setSuccess] = React.useState(false);
+  const router = useRouter();
+
+  React.useEffect(() => {
+    let timeout;
+    if (success) {
+      timeout = setTimeout(() => setSuccess(false), 3000);
+    }
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [success]);
+
+  async function handleAddProductToCart() {
+    try {
+      setLoading(true);
+      const url = `${baseUrl}/api/cart`;
+      const payload = { quantity, productId };
+      const token = cookie.get('token');
+      const headers = { headers: { Authorization: token } };
+      await axios.put(url, payload, headers);
+      setSuccess(true);
+    } catch (error) {
+      catchErrors(error, window.alert);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <Input
       type="number"
       min="1"
       placeholder="Quantity"
-      value={1}
-      action={{
-        color: "orange",
-        content: "Add to Cart",
-        icon: "plus cart"
-      }}
+      value={quantity}
+      onChange={(event) => setQuantity(Number(event.target.value))}
+      action={
+        user && success
+          ? {
+              color: 'blue',
+              content: 'Item Added!',
+              icon: 'plus cart',
+              disabled: true,
+            }
+          : user
+          ? {
+              color: 'orange',
+              content: 'Add to Cart',
+              icon: 'plus cart',
+              loading,
+              disabled: loading,
+              onClick: handleAddProductToCart,
+            }
+          : {
+              color: 'blue',
+              content: 'Sign Up To Purchase',
+              icon: 'signup',
+              onClick: () => router.push('/signup'),
+            }
+      }
     />
   );
 }
--- pages/cart.js
+++ pages/cart.js
@@ -1,8 +1,12 @@
 import { Segment } from 'semantic-ui-react';
 import CartItemList from '../components/Cart/CartItemList';
 import CartSummary from '../components/Cart/CartSummary';
+import { parseCookies } from 'nookies';
+import axios from 'axios';
+import baseUrl from '../utils/baseUrl';
 
-function Cart() {
+function Cart({ products }) {
+  console.log(products);
   return (
     <Segment>
       <CartItemList />
@@ -10,5 +14,16 @@ function Cart() {
     </Segment>
   );
 }
+
+Cart.getInitialProps = async (ctx) => {
+  const { token } = parseCookies(ctx);
+  if (!token) {
+    return { products: [] };
+  }
+  const url = `${baseUrl}/api/cart`;
+  const payload = { headers: { Authorization: token } };
+  const response = await axios.get(url, payload);
+  return { products: response.data };
+};
 
 export default Cart;
