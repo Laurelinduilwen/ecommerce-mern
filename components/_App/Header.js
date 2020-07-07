8541ba9b68f0fe1d4c2f29fa0f60f604b2852ba4
Laurelinduilwen <ladylaurelin@hotmail.com>
2020-07-07 23:57:22 +0200

    added login functionality

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -2,14 +2,17 @@ import { Menu, Container, Image, Icon } from 'semantic-ui-react';
 import Link from 'next/link';
 import Router, { useRouter } from 'next/router';
 import NProgress from 'nprogress';
+import { handleLogout } from '../../utils/auth';
 
 Router.onRouteChangeStart = () => NProgress.start();
 Router.onRouteChangeComplete = () => NProgress.done();
 Router.onRouteChangeError = () => NProgress.done();
 
-function Header() {
+function Header({ user }) {
   const router = useRouter();
-  const user = false;
+  const isRoot = user && user.role === 'root';
+  const isAdmin = user && user.role === 'admin';
+  const isRootOrAdmin = isRoot || isAdmin;
 
   function isActive(route) {
     return route === router.pathname;
@@ -32,7 +35,7 @@ function Header() {
           </Menu.Item>
         </Link>
 
-        {user && (
+        {isRootOrAdmin && (
           <Link href="/create">
             <Menu.Item header active={isActive('/create')}>
               <Icon name="add square" size="large" />
@@ -50,7 +53,7 @@ function Header() {
               </Menu.Item>
             </Link>
 
-            <Menu.Item header>
+            <Menu.Item onClick={handleLogout} header>
               <Icon name="sign out" size="large" />
               Logout
             </Menu.Item>
--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -1,10 +1,10 @@
-import Head from "next/head";
-import { Container } from "semantic-ui-react";
+import Head from 'next/head';
+import { Container } from 'semantic-ui-react';
 
-import Header from "./Header";
-import HeadContent from "./HeadContent";
+import Header from './Header';
+import HeadContent from './HeadContent';
 
-function Layout({ children }) {
+function Layout({ children, user }) {
   return (
     <>
       <Head>
@@ -18,8 +18,8 @@ function Layout({ children }) {
         />
         <title>ReactReserve</title>
       </Head>
-      <Header />
-      <Container text style={{ paddingTop: "1em" }}>
+      <Header user={user} />
+      <Container text style={{ paddingTop: '1em' }}>
         {children}
       </Container>
     </>
