878c450d276928753f7e43009e5b744ca0597516
Laurelinduilwen <ladylaurelin@hotmail.com>
2020-07-08 05:18:47 +0200

    still trying to fix deployment issues.....

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,4 +1,6 @@
 const baseUrl =
-  process.env.NODE_ENV === 'production' ? 'hyf-mern-ecommerce.vercel.app' : 'http://localhost:3000';
+  process.env.NODE_ENV === 'production'
+    ? 'https://ecommerce-mern-tau.vercel.app/'
+    : 'http://localhost:3000';
 
 export default baseUrl;
